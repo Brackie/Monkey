7b8969789ae2a67639e7c79d7d387d783aca2fcb
Brackie <muimikieleko@gmail.com>
2020-01-22 17:36:04 +0300

    Added default block in switch to handle identifiers

--- lexer.c
+++ lexer.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -11,6 +12,52 @@ void newLexer(Lexer *l, char *input){
 	readChar(l);
 }
 
+void nextToken(Lexer *l, Token *t){
+	switch(*l->ch){
+		case '=':
+			newToken(ASSIGN, l->ch, t);
+			break;
+		case ';':
+			newToken(SEMICOLON, l->ch, t);
+			break;
+		case '(':
+			newToken(LPAREN, l->ch, t);
+			break;
+		case ')':
+			newToken(RPAREN, l->ch, t);
+			break;
+		case ',':
+			newToken(COMMA, l->ch, t);
+			break;
+		case '+':
+			newToken(PLUS, l->ch, t);
+			break;
+		case '{':
+			newToken(LBRACE, l->ch, t);
+			break;
+		case '}':
+			newToken(RBRACE, l->ch, t);
+			break;
+		case 0:
+			newToken(EOF, "", t);
+			break;
+		default:
+			if (isLetter(*l->ch)){
+				Identifier i;
+				readIdentifier(l, &i);
+				int type = lookUpIdentifier(&i);
+				newToken(type, l->ch, t);
+				return;
+			}
+			else{
+				newToken(ILLEGAL, l->ch, t);
+				return;
+			}
+
+	}
+	readChar(l);
+}
+
 void readChar(Lexer *l){
 	if(l->readPosition >= strlen(l->input))
 		l->ch = 0;
@@ -19,3 +66,4 @@ void readChar(Lexer *l){
 	l->position = l->readPosition;
 	l->readPosition += 1;
 }
+
