54bb5d6f6c57180d1705e99f2b86f22c3cb8a803
Brackie <muimikieleko@gmail.com>
2020-01-22 17:38:28 +0300

    Added function to return identifier type and a function to test if a character is a letter

--- token.c
+++ token.c
@@ -1,44 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include <ctype.h>
 #include "monkey.h"
 
 
-void nextToken(Lexer *l, Token *t){
-	switch(l->ch){
-		case '=':
-			newToken(ASSIGN, l->ch, t);
-			break;
-		case ';':
-			newToken(SEMICOLON, l->ch, t);
-			break;
-		case '(':
-			newToken(LPAREN, l->ch, t);
-			break;
-		case ')':
-			newToken(RPAREN, l->ch, t);
-			break;
-		case ',':
-			newToken(COMMA, l->ch, t);
-			break;
-		case '+':
-			newToken(PLUS, l->ch, t);
-			break;
-		case '{':
-			newToken(LBRACE, l->ch, t);
-			break;
-		case '}':
-			newToken(RBRACE, l->ch, t);
-			break;
-		case 0:
-			newToken(EOF, "", t);
-			break;
-	}
-}
+const char *LET_IDENT = "let";
+const char *FN_IDENT = "fn";
 
 void newToken(int type, char *lit, Token *t){
 	t->tokenType = type;
 	t->literal = lit;
 }
 
+int isLetter(char ch){
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_';
+}
+
+void readIdentifier(Lexer *l, Identifier *i){
+	int j = 0;
+	i->start = l->ch;
+	while (isLetter(*l->ch)){
+		i->identifier[j] = *l->ch;
+		j++;
+		readChar(l);
+	}
+	i->end = l->ch - 1;
+}
+
+int lookUpIdentifier(Identifier *i){
+	int type = IDENTIFIER;
+	printf("Start: %c, End: %c, Identifier: %s, Size: %d\n", *i->start, *i->end, i->identifier, (sizeof(i->identifier)/sizeof(i->identifier[0])));
+	if (!strcmp(i->identifier, LET_IDENT))
+		return LET;
+	else if (!strcmp(i->identifier, FN_IDENT))
+		return FUNCTION;
+	printf("Token Type: %d\n", type);
+	return type;
+}
 
